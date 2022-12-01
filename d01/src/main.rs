d6d7492d43724b1b887183033ff5cfb156f81377
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-12-01 00:09:44 -0800

    Day 1, problem 2 (#2)

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,4 +1,6 @@
 use std::{
+    cmp::Reverse,
+    collections::BinaryHeap,
     fs::File,
     io::{self, BufRead},
     path::Path,
@@ -13,21 +15,32 @@ where
 }
 
 fn main() {
-    let mut max_calories = 0;
+    let mut calorie_counts = BinaryHeap::new();
+    let max_retained_counts = 3;
+
     let mut curr_calories = 0;
+
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
             let line = line.unwrap();
             if line.is_empty() {
-                if curr_calories > max_calories {
-                    max_calories = curr_calories;
-                }
+                calorie_counts.push(Reverse(curr_calories));
                 curr_calories = 0;
+
+                while calorie_counts.len() > max_retained_counts {
+                    calorie_counts.pop();
+                }
             } else {
                 let calories = line.parse::<i32>().unwrap();
                 curr_calories = curr_calories + calories;
             }
         }
     }
-    println!("Max calories is {}", max_calories)
+
+    let total_calories: i32 = calorie_counts.iter().map(|Reverse(x)| x).sum();
+
+    println!(
+        "Total calories of top {} is {}",
+        max_retained_counts, total_calories
+    )
 }
