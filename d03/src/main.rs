fa01c430da0c4b9eeaf7799daf15c7434690e0f6
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-12-10 16:55:21 -0800

    Done day 3, problem 2

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,33 +1,36 @@
+use itertools::Itertools;
 use std::collections::HashSet;
-
 use utils::read_lines;
+
+fn get_item_priority(item: char) -> u32 {
+    let mut item_priority = item as u32;
+
+    if item_priority >= 'a' as u32 {
+        item_priority -= 'a' as u32;
+    } else {
+        item_priority -= 'A' as u32;
+        item_priority += 26;
+    }
+    item_priority += 1;
+    item_priority
+}
 
 fn main() {
     let mut sum = 0;
     let lines = read_lines("input.txt").unwrap();
-    for line in lines {
-        let line = line.unwrap();
-        let (lhs, rhs) = line.split_at(line.len() / 2);
-        let lhs_set = lhs.chars().collect::<HashSet<char>>();
-        let rhs_set = rhs.chars().collect::<HashSet<char>>();
+    let elves_per_group = 3;
 
-        let mut overlap = lhs_set.intersection(&rhs_set);
-        let overlapping_item = *(overlap.nth(0).unwrap());
+    for group in &lines.chunks(elves_per_group) {
+        let mut knapsacks = group.map(|ks| ks.unwrap().chars().collect::<HashSet<char>>());
+        let first_knapsack = knapsacks.nth(0).unwrap();
+        let common_items = knapsacks.fold(first_knapsack, |l, r| {
+            l.intersection(&r).map(|x| *x).collect::<HashSet<char>>()
+        });
+        assert_eq!(common_items.len(), 1);
+        let badge = *common_items.iter().nth(0).unwrap();
 
-        let mut overlapping_item_priority = overlapping_item as u32;
-        assert_eq!(overlap.count(), 0);
-
-        if overlapping_item_priority >= 'a' as u32 {
-            overlapping_item_priority -= 'a' as u32;
-        } else {
-            overlapping_item_priority -= 'A' as u32;
-            overlapping_item_priority += 26;
-        }
-        overlapping_item_priority += 1;
-
-        println!("{} -> {}", overlapping_item, overlapping_item_priority);
-
-        sum += overlapping_item_priority;
+        let priority = get_item_priority(badge);
+        sum += priority;
     }
 
     println!("{}", sum)
